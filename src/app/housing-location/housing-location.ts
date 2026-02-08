cd2c838e0d60354e60d06fbbf7b2c14f33933f25
siuupriyanshu <mandalpriyanshu22@gmail.com>
2026-02-08 23:57:15 +0545

    add dynamic values to tempelates

--- src/app/housing-location/housing-location.ts
+++ src/app/housing-location/housing-location.ts
@@ -5,9 +5,17 @@ import { HousingLocationInfo } from '../housinglocation';
   selector: 'app-housing-location',
   imports: [],
   template: `
-    <p>
-      housing-location works!
-    </p>
+...
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation().photo"
+        alt="Exterior photo of {{ housingLocation().name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation().name }}</h2>
+      <p class="listing-location">{{ housingLocation().city }}, {{ housingLocation().state }}</p>
+    </section>
   `,
   styles: ``,
 })
